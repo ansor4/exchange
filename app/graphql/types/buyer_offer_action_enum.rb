ff5566ba921291bf9084ec5405d610a8cfa30775
Sepand Ansari <sepans@sepans.com>
2021-06-04 10:04:34 -0400

    typos
    Co-authored-by: Erik <erikdstock@users.noreply.github.com>

--- app/graphql/types/buyer_offer_action_enum.rb
+++ app/graphql/types/buyer_offer_action_enum.rb
@@ -1,8 +1,8 @@
 class Types::BuyerOfferActionEnum < Types::BaseEnum
   value 'PAYMENT_FAILED', "Buyer's payment failed", value: 'PAYMENT_FAILED'
-  value 'RECEIVED_OFFER_CONFIRM_NEEDED', 'Buyer received a counter offer needs to confrim tax and shipping', value: 'RECEIVED_OFFER_CONFIRM_NEEDED'
+  value 'RECEIVED_OFFER_CONFIRM_NEEDED', 'Buyer received a counter offer needs to confirm tax and shipping', value: 'RECEIVED_OFFER_CONFIRM_NEEDED'
   value 'OFFER_ACCEPTED_CONFIRM_NEEDED', "Buyer's offer accepted needs to confirm tax and shipping", value: 'OFFER_ACCEPTED_CONFIRM_NEEDED'
-  value 'RECEIVED_OFFER', 'Buyer recieved a counter offer', value: 'RECEIVED_OFFER'
+  value 'RECEIVED_OFFER', 'Buyer received a counter offer', value: 'RECEIVED_OFFER'
   value 'OFFER_ACCEPTED', "Buyer's offer is accepted and final", value: 'OFFER_ACCEPTED'
   value 'PROVISIONAL_OFFER_ACCEPTED', 'Provisional offer is accepted and tax/shipping confirmed', value: 'PROVISIONAL_OFFER_ACCEPTED'
 end
