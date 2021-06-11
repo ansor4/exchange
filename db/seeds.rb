7406a3e6c8c59fe48acabeedb24ddbf2054837bb
Dagmar Kutz <dagmar.kutz@artsymail.com>
2021-06-11 15:25:35 +0200

    chore: changed artwork because we need a shipable category

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 # buyer_id is test user with e-mail "test@artsymail.com". The test user is available on production.
 order = Order.create(state: 'pending', payment_method: 'credit card', currency_code: 'USD', mode: 'offer', buyer_id: '55539bfd7261692b13530100')
-# "Test artwork" of "Commerce Test Partner". It is available on production so it will be copied over to staging each week.
-LineItem.create(artwork_id: '60a675b08287e200135a4784', artwork_version_id: '60c27e0cf448330013d340a5', order: order)
+# "test" of "Folio Test Partner". It is available on production so it will be copied over to staging each week.
+LineItem.create(artwork_id: '54c0310772616961ff030700', artwork_version_id: '60c27e0cf448330013d340a5', order: order)
